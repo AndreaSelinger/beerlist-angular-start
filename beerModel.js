46524d59d83572236a2518595601d1b562fa3a09
Andrea Selinger <Seli@MacBook-Pro-4.local>
2017-10-18 11:48:17 +0300

    set up server

--- beerModel.js
+++ beerModel.js
@@ -1,1 +1,15 @@
+var mongoose = require ('mongoose');
+var Schema = mongoose.Schema;
 
+var beerSchema = new Schema({
+    name: { type: String },
+    style: { type: String },
+    image_url: { type: String },
+    abv: { type: Number },
+    ratingTotal: { type: Number },
+    numberOfRatings: { type: Number }
+  });
+  
+  var Beer = mongoose.model("Beer", beerSchema);
+  
+  module.exports = Beer;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/beers');
+
+var Beer = require('./beerModel');
 
 var app = express();
 
@@ -7,6 +12,73 @@ app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/beers', function (req, res, next) {
+  Beer.find(function (error, beers) {
+    if (error) {
+      return next(error);
+    } else {
+      return res.send(beers);
+    }
+  });
+});
+
+app.post('/beers', function(req, res, next) {
+  Beer.create(req.body, function(err, beer) {
+    if (err) {
+      return next(err);
+    } else {
+      return res.send(beer);
+    }
+  });
+});
+
+app.delete('/beers/:id', function(req, res, next) {
+  Beer.findByIdAndRemove(req.params.id, function(err, beer) {
+    if (err) {
+      return next(err);
+    } else {
+      res.send(beer);
+    }
+  });
+});
+
+app.put('/beers/:beerId', function(req, res, next) {
+  Beer.findByIdAndUpdate(req.params.beerId, req.body, { new: true }, function(err, beer) {
+    if (err) {
+      return next(err);
+    }
+    res.send(beer);
+  });
+});
+
+app.post('/beers/:id/ratings', function(req, res, next) {
+  
+  var update = { $push: { ratings: req.body.rating } };
+  Beer.findByIdAndUpdate(req.params.id, update, { new: true }, function(err, beer) {
+    if (err) {
+      return next(err);
+    }
+    res.send(beer);
+  });
+});
+
+// error handler to catch 404 and forward to main error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// main error handler
+// warning - not for use in production code!
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err
+  });
+});
 
 app.listen(8000, function() {
   console.log("yo yo yo, on 8000!!")
